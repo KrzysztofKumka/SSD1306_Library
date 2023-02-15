2a74b911db9f6e8079b6dfcfd6f702fed3a643c2
Krzysztof Kumka <73432969+KrzysztofKumka@users.noreply.github.com>
2023-02-15 15:39:14 +0100

    fixed display
    all white/black display

--- SSD1306/Core/Src/SSD1306_OLED.c
+++ SSD1306/Core/Src/SSD1306_OLED.c
@@ -1,10 +1,3 @@
-/*
- * SSD1306_OLED.c
- *
- *  Created on: Feb 5, 2023
- *      Author: krzysztof
- */
-
 #include "main.h"
 #include "SSD1306_OLED.h"
 #include "string.h"
@@ -13,43 +6,52 @@ I2C_HandleTypeDef *oled_i2c;
 
 static uint8_t buffer[SSD1306_BUFFER_SIZE];
 
-void SSD1306_Command(uint8_t Command) {
+void SSD1306_Command(uint8_t Command)
+{
 	HAL_I2C_Mem_Write(oled_i2c, (SSD1306_ADDRESS<<1), 0x00, 1, &Command, 1, SSD1306_TIMEOUT);
 }
 
-void SSD1306_Data(uint8_t Data, uint16_t Size) {
-	HAL_I2C_Mem_Write(oled_i2c, (SSD1306_ADDRESS<<1), 0x40, 1, &Data, Size, SSD1306_TIMEOUT);
+void SSD1306_Data(uint8_t *Data, uint16_t Size)
+{
+	HAL_I2C_Mem_Write(oled_i2c, (SSD1306_ADDRESS<<1), 0x40, 1, Data, Size, SSD1306_TIMEOUT);
 }
 
 //
 // Functions
 //
 
-void SSD1306_Clear(uint8_t Color) {
-	switch(Color) {
+void SSD1306_Clear(uint8_t Color)
+{
+	switch(Color)
+	{
 	case WHITE:
 		memset(buffer, 0xFF, SSD1306_BUFFER_SIZE);
 		break;
+
 	case BLACK:
 		memset(buffer, 0x00, SSD1306_BUFFER_SIZE);
 		break;
 	}
 }
 
-void SSD1306_Display(void) {
+void SSD1306_Display(void)
+{
 	SSD1306_Command(SSD1306_PAGEADDR);
-	SSD1306_Command(0x00);					// Page start address
-	SSD1306_Command(0xFF);					// Page end address
+	SSD1306_Command(0);                      	// Page start address
+	SSD1306_Command(0xFF);                   	// Page end
 	SSD1306_Command(SSD1306_COLUMNADDR);
-	SSD1306_Command(0x00);					// Column start address
-	SSD1306_Command(SSD1306_LCDWIDTH - 1);	// Column end address
+	SSD1306_Command(0); 						// Column start address
+	SSD1306_Command(SSD1306_LCDWIDTH - 1); 		// Column end address
 
 	SSD1306_Data(buffer, SSD1306_BUFFER_SIZE);
 }
 
-void SSD1306_Init(I2C_HandleTypeDef *i2c) {
+void SSD1306_Init(I2C_HandleTypeDef *i2c)
+{
 	oled_i2c = i2c;
+
 	SSD1306_Command(SSD1306_DISPLAYOFF);
+
 	SSD1306_Command(SSD1306_SETDISPLAYCLOCKDIV);
 	SSD1306_Command(0x80);
 
@@ -60,10 +62,10 @@ void SSD1306_Init(I2C_HandleTypeDef *i2c) {
 	SSD1306_Command(SSD1306_SETSTARTLINE);
 
 	SSD1306_Command(SSD1306_CHARGEPUMP);
-	SSD1306_Command(0x14);						// Non external power source
+	SSD1306_Command(0x14);
 
-	SSD1306_Command(SSD1306_MEMORYMODE);
-	SSD1306_Command(0x00);
+	SSD1306_Command(SSD1306_MEMORYMODE); // 0x20
+	SSD1306_Command(0x00); // 0x0 act like ks0108
 	SSD1306_Command(SSD1306_SEGREMAP | 0x1);
 	SSD1306_Command(SSD1306_COMSCANDEC);
 
@@ -72,14 +74,14 @@ void SSD1306_Init(I2C_HandleTypeDef *i2c) {
 	SSD1306_Command(SSD1306_SETCONTRAST);
 	SSD1306_Command(0xFF);
 
-	SSD1306_Command(SSD1306_SETPRECHARGE);
-	SSD1306_Command(0xF1); 	// non external power source
+	SSD1306_Command(SSD1306_SETPRECHARGE); // 0xd9
+	SSD1306_Command(0xF1);
 
-	SSD1306_Command(SSD1306_SETVCOMDETECT);
+	SSD1306_Command(SSD1306_SETVCOMDETECT); // 0xDB
 	SSD1306_Command(0x40);
-	SSD1306_Command(SSD1306_DISPLAYALLON_RESUME);
-	SSD1306_Command(SSD1306_NORMALDISPLAY);
+	SSD1306_Command(SSD1306_DISPLAYALLON_RESUME); // 0xA4
+	SSD1306_Command(SSD1306_NORMALDISPLAY);       // 0xA6
 	SSD1306_Command(SSD1306_DEACTIVATE_SCROLL);
-	SSD1306_Command(SSD1306_DISPLAYON);
 
+	SSD1306_Command(SSD1306_DISPLAYON); // Main screen turn on
 }
